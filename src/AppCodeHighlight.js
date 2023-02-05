27c6977773d37081dc006802da313485f82fad1b
Mitch <bigmitchsystems@gmail.com>
2023-02-05 07:43:39 +0200

    updated initial code base

--- src/AppCodeHighlight.js
+++ src/AppCodeHighlight.js
@@ -1,32 +1,32 @@
-// // import React, { useEffect, useRef } from 'react';
-// // import Prism from 'prismjs/components/prism-core';
-// import 'prismjs/components/prism-clike';
-// import 'prismjs/components/prism-markup';
-// import 'prismjs/components/prism-javascript';
-// import 'prismjs/components/prism-css';
-// import 'prismjs/components/prism-scss';
+import React, { useEffect, useRef } from 'react';
+import Prism from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-scss';
 
-// const AppCodeHighlight = (props) => {
-//     // const codeElement = useRef(null);
+const AppCodeHighlight = (props) => {
+    const codeElement = useRef(null);
 
-//     // useEffect(() => {
-//     //     if (Prism) {
-//     //         Prism.highlightElement(codeElement.current);
-//     //     }
-//     // }, []);
+    useEffect(() => {
+        if (Prism) {
+            Prism.highlightElement(codeElement.current);
+        }
+    }, []);
 
-//     // return (
-//     //     <pre style={props.style}>
-//     //         <code ref={codeElement} className={`language-${props.lang}`}>
-//     //             {props.children}&nbsp;
-//     //         </code>
-//     //     </pre>
-//     // );
-// };
+    return (
+        <pre style={props.style}>
+            <code ref={codeElement} className={`language-${props.lang}`}>
+                {props.children}&nbsp;
+            </code>
+        </pre>
+    );
+};
 
-// // AppCodeHighlight.defaultProps = {
-// //     lang: 'jsx',
-// //     style: null
-// // };
+AppCodeHighlight.defaultProps = {
+    lang: 'jsx',
+    style: null
+};
 
-// export default AppCodeHighlight;
+export default AppCodeHighlight;
